8269d1480d06e278c1100ce42b93e6395b1d1568
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-12-09 12:26:12 +0800

    14_queue_game_multi_input

---
+++ 14_queue_game_multi_input/02_nwatch_game_freertos/nwatch/typedefs.h
@@ -0,0 +1,43 @@
+/*
+ * Project: N|Watch
+ * Author: Zak Kemble, contact@zakkemble.co.uk
+ * Copyright: (C) 2013 by Zak Kemble
+ * License: GNU GPL v3 (see License.txt)
+ * Web: http://blog.zakkemble.co.uk/diy-digital-wristwatch/
+ */
+
+#ifndef TYPEDEFS_H_
+#define TYPEDEFS_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef uint8_t byte;
+typedef uint16_t uint;
+typedef uint32_t ulong;
+
+#define UPT_MOVE_NONE	0
+#define UPT_MOVE_RIGHT	1
+#define UPT_MOVE_LEFT	2
+
+#define PROGMEM
+
+struct input_data
+{
+    uint32_t dev;
+    uint32_t val;         /*UPT_MOVE_NONE,UPT_MOVE_RIGHT,UPT_MOVE_LEFT*/
+};
+
+struct rotary_data
+{
+    int32_t cnt;
+    int32_t speed;
+};
+
+
+// Quick and easy macro for a for loop
+#define LOOP(count, var) for(byte var=0;var<count;var++)
+
+
+
+#endif /* TYPEDEFS_H_ */
